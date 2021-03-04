6cc094b83181249993a09dfc613391901e735d57
gpanice <giovanni.panice@lastminute.com>
2021-03-04 23:28:43 +0100

    add tool window

---
+++ src/main/kotlin/com/github/idell/gitlabconnect/ui/GitlabConnectToolWindow.kt
@@ -0,0 +1,11 @@
+package com.github.idell.gitlabconnect.ui
+
+import com.intellij.openapi.wm.ToolWindow
+import javax.swing.JComponent
+
+class GitlabConnectToolWindow(toolWindow: ToolWindow) {
+    fun geContent(): JComponent? {
+        TODO("Not yet implemented")
+    }
+
+}
---
+++ src/main/kotlin/com/github/idell/gitlabconnect/ui/GitlabConnectToolWindowFactory.kt
@@ -0,0 +1,18 @@
+package com.github.idell.gitlabconnect.ui
+
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.wm.ToolWindow
+import com.intellij.openapi.wm.ToolWindowFactory
+import com.intellij.ui.content.ContentFactory
+
+
+class GitlabConnectToolWindowFactory: ToolWindowFactory {
+
+    override fun createToolWindowContent(project: Project, toolWindow: ToolWindow) {
+        val gitlabConnectToolWindow = GitlabConnectToolWindow(toolWindow)
+        val contentFactory = ContentFactory.SERVICE.getInstance()
+        val content = contentFactory.createContent(gitlabConnectToolWindow.geContent(), "Gitlab", false)
+        toolWindow.contentManager.addContent(content)
+    }
+
+}
