9f49bbe625da2138630649f02afa58e67a2ed94f
idelloro <ivan.delloro@lastminute.com>
2021-03-26 12:22:15 +0100

    [add_window] WIP: handling connection error

--- src/test/kotlin/com/github/idell/gitlabconnect/services/IssueServiceTest.kt
+++ src/test/kotlin/com/github/idell/gitlabconnect/services/IssueServiceTest.kt
@@ -10,26 +10,33 @@ internal class GitlabTestIT {
 
     @Test
     internal fun `given a full path project, returns a valid id`() {
-        val gitlabConfiguration = GitlabConfiguration("https://gitlab.lastminute.com", "k761oxXGdsyzHR_49DKJ")
-        val apiClient = ApiClient(gitlabConfiguration)
+        val apiClient = GitlabConnectDataRetriever(GitlabConnectApi(GitlabConfiguration("https://gitlab.lastminute.com",
+                                                                                        "k761oxXGdsyzHR_49DKJ")))
         val first = apiClient.getId("obi1/order-manager")
 
         assertEquals(2455, first)
     }
 
+    @Test
+    internal fun `given a wrong full path project, returns an empty `(){
+
+//        val apiClient = GitlabConnectDataRetriever(GitlabConnectApi(GitlabConfiguration("https://gitlab.lastminute.com",
+//                                                                                        "k761oxXGdsyzHR_49DKJ")))
+
+    }
+
 }
 
-class ApiClient(private val gitlabConfiguration: GitlabConfiguration) {
-
-    private val gitLabApi: GitLabApi = GitLabApi(GitLabApi.ApiVersion.V4,
-                                                 this.gitlabConfiguration.host,
-                                                 this.gitlabConfiguration.token)
+class GitlabConnectDataRetriever(private val gitlabConnectApi: GitlabConnectApi) {
 
     fun getId(pathWitNamespace: String): Int? {
-
-        val projects: List<Project> = gitLabApi.projectApi.getProjects(pathWitNamespace)
+        val projects: List<Project> = gitlabConnectApi.gitLabApi.projectApi.getProjects(pathWitNamespace)
         return projects.first { project -> project.pathWithNamespace.equals(pathWitNamespace) }.id
     }
+}
+
+class GitlabConnectApi(gitlabConfiguration: GitlabConfiguration) {
+    val gitLabApi = GitLabApi(gitlabConfiguration.host,gitlabConfiguration.token)
 }
 
 data class GitlabConfiguration(val host: String, val token: String)
