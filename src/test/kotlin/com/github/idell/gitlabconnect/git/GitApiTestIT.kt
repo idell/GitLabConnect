3bf8cb671568006a1247a582460258ad6ae31298
gpanice <giovanni.panice@lastminute.com>
2021-04-02 10:39:04 +0200

    fix broken test

--- src/test/kotlin/com/github/idell/gitlabconnect/git/GitApiTestIT.kt
+++ src/test/kotlin/com/github/idell/gitlabconnect/git/GitApiTestIT.kt
@@ -3,6 +3,8 @@ package com.github.idell.gitlabconnect.git
 import org.eclipse.jgit.api.Git
 import org.eclipse.jgit.storage.file.FileRepositoryBuilder
 import org.eclipse.jgit.transport.URIish
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import java.io.File
 import kotlin.test.assertEquals
@@ -10,28 +12,45 @@ import kotlin.test.assertTrue
 
 internal class GitApiTestIT {
 
+    private val exampleWithRemotes: File = File("./exampleWithRemote")
+    private val exampleWithoutRemotes = File("src/test/resources/exampleWithoutRemote")
+
+    @BeforeEach
+    internal fun setUp() {
+        createRepository(exampleWithRemotes, "origin", "git@gitlab.example.com:namespace/project.git")
+        Git.init()
+            .setDirectory(exampleWithoutRemotes)
+            .call()
+    }
+
+    @AfterEach
+    internal fun tearDown() {
+        exampleWithRemotes.deleteRecursively()
+        exampleWithoutRemotes.deleteRecursively()
+    }
+
+    private fun createRepository(file: File, remoteName: String, remoteUrl: String) {
+        Git.init()
+            .setDirectory(file)
+            .call()
+            .remoteAdd()
+            .setName(remoteName)
+            .setUri(URIish(remoteUrl))
+            .call()
+    }
+
+
     @Test
     internal fun `given a git folder, retrieve information related remotes`() {
 
-        val file = File("./exampleWithRemote")
-
-        Git.init()
-            .setDirectory(File(file.absoluteFile.toString() + "/.git"))
-            .call()
-            .remoteAdd()
-            .setName("origin")
-            .setUri(URIish("git@gitlab.example.com:namespace/project.git"))
-            .call()
-
         val repository = FileRepositoryBuilder()
-            .setGitDir(file)
+            .setGitDir(File(exampleWithRemotes.absoluteFile.toString() + "/.git"))
             .readEnvironment()
             .findGitDir()
             .setMustExist(true)
             .build()
 
-        val gitApi = GitApi(repository)
-        val remotes = gitApi.findRemotes()
+        val remotes = GitApi(repository).findRemotes()
 
         assertEquals("origin", remotes[0].name)
         assertEquals("git@gitlab.example.com:namespace/project.git", remotes[0].address)
@@ -41,7 +60,7 @@ internal class GitApiTestIT {
     internal fun `given a git folder without remotes, return an empty list`() {
 
         val repository = FileRepositoryBuilder()
-            .setGitDir(File("src/test/resources/exampleWithoutRemote"))
+            .setGitDir(File(exampleWithoutRemotes.absoluteFile.toString() + "/.git"))
             .readEnvironment()
             .findGitDir()
             .setMustExist(true)
