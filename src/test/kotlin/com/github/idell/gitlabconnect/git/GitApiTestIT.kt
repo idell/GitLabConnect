81e9aefd40c55130f99f1b6f387e3ec780e16540
gpanice <giovanni.panice@lastminute.com>
2021-04-02 10:15:01 +0200

    WIP

--- src/test/kotlin/com/github/idell/gitlabconnect/git/GitApiTestIT.kt
+++ src/test/kotlin/com/github/idell/gitlabconnect/git/GitApiTestIT.kt
@@ -1,6 +1,8 @@
 package com.github.idell.gitlabconnect.git
 
+import org.eclipse.jgit.api.Git
 import org.eclipse.jgit.storage.file.FileRepositoryBuilder
+import org.eclipse.jgit.transport.URIish
 import org.junit.jupiter.api.Test
 import java.io.File
 import kotlin.test.assertEquals
@@ -11,8 +13,18 @@ internal class GitApiTestIT {
     @Test
     internal fun `given a git folder, retrieve information related remotes`() {
 
+        val file = File("./exampleWithRemote")
+
+        Git.init()
+            .setDirectory(File(file.absoluteFile.toString() + "/.git"))
+            .call()
+            .remoteAdd()
+            .setName("origin")
+            .setUri(URIish("git@gitlab.example.com:namespace/project.git"))
+            .call()
+
         val repository = FileRepositoryBuilder()
-            .setGitDir(File("src/test/resources/exampleWithRemote"))
+            .setGitDir(file)
             .readEnvironment()
             .findGitDir()
             .setMustExist(true)
