c20af6e3d082ee0503ff580f678586960ae78d18
gpanice <giovanni.panice@lastminute.com>
2021-04-03 21:10:06 +0200

    fix linting issues

--- src/test/kotlin/com/github/idell/gitlabconnect/gitlab/issues/GitlabConnectDataRetrieverTest.kt
+++ src/test/kotlin/com/github/idell/gitlabconnect/gitlab/issues/GitlabConnectDataRetrieverTest.kt
@@ -1,7 +1,15 @@
 package com.github.idell.gitlabconnect.gitlab.issues
 
 import com.github.idell.gitlabconnect.exception.GitlabConnectException
-import com.github.idell.gitlabconnect.gitlab.*
+import com.github.idell.gitlabconnect.gitlab.ConnectApi
+import com.github.idell.gitlabconnect.gitlab.ConnectDataRetriever
+import com.github.idell.gitlabconnect.gitlab.GitlabConnectDataRetriever
+import com.github.idell.gitlabconnect.gitlab.ProjectSearch
+import com.github.idell.gitlabconnect.gitlab.ProjectInfo
+import com.github.idell.gitlabconnect.gitlab.Issue
+import com.github.idell.gitlabconnect.gitlab.UserInfo
+import com.github.idell.gitlabconnect.gitlab.GitlabConnectApi
+import com.github.idell.gitlabconnect.gitlab.GitlabConfiguration
 import org.gitlab4j.api.models.Namespace
 import org.gitlab4j.api.models.Project
 import org.gitlab4j.api.models.User
@@ -17,7 +25,7 @@ import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 import org.junit.jupiter.api.extension.RegisterExtension
-import java.util.*
+import java.util.Optional
 import kotlin.test.assertFalse
 import org.gitlab4j.api.models.Issue as GitlabIssue
 
