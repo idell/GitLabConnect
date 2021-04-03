b9d8a7068dbddb8596c148f370ffdf2455644980
idelloro <ivan.delloro@lastminute.com>
2021-04-03 15:52:32 +0200

    [token_configuration] Testing settings form

--- src/test/kotlin/com/github/idell/gitlabconnect/gitlab/config/GitlabConnectSettingsComponentTest.kt
+++ src/test/kotlin/com/github/idell/gitlabconnect/gitlab/config/GitlabConnectSettingsComponentTest.kt
@@ -1,13 +1,25 @@
 package com.github.idell.gitlabconnect.gitlab.config
 
+import com.intellij.ui.components.JBLabel
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
+import javax.swing.JPanel
+import javax.swing.JPasswordField
 
 internal class GitlabConnectSettingsComponentTest {
 
     @Test
     internal fun `construct a component with form`() {
         val panel = GitlabConnectSettingsComponent().getPanel()
-//
-//        assertEquals(3, panel.components)
+
+        val jLabels: List<JBLabel> = panel.components.filterIsInstance<JBLabel>()
+        assertThat(jLabels.filter { it.text ==  "Gitlab host: "}).isNotEmpty
+        assertThat(jLabels.filter { it.text ==  "Gitlab connection token:"}).isNotEmpty
+
+        val jPanels: List<JPanel> = panel.components.filterIsInstance<JPanel>()
+        assertThat(jPanels.size).isEqualTo(2)
+
+        val jPasswordFields: List<JPasswordField> = panel.components.filterIsInstance<JPasswordField>()
+        assertThat(jPasswordFields).isNotEmpty
     }
 }
