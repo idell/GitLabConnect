d5d27db46acc436a22facd2419ea740319b64d65
gpanice <giovanni.panice@lastminute.com>
2021-04-03 21:52:12 +0200

    fix linting

--- src/main/kotlin/com/github/idell/gitlabconnect/services/MyProjectService.kt
+++ src/main/kotlin/com/github/idell/gitlabconnect/services/MyProjectService.kt
@@ -1,11 +1,10 @@
 package com.github.idell.gitlabconnect.services
 
-import com.github.idell.gitlabconnect.GitlabConnectBundle
 import com.intellij.openapi.project.Project
 
 class MyProjectService(project: Project) {
 
     init {
-        //println(GitlabConnectBundle.message("projectService", project.name))
+        // println(GitlabConnectBundle.message("projectService", project.name))
     }
 }
