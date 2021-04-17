1b33984a28ae6ba052a6db9babb2438b65ae3933
idelloro <ivan.delloro@lastminute.com>
2021-04-17 09:00:35 +0200

    [14] WIP: ignored wip test

--- src/test/kotlin/com/github/idell/gitlabconnect/ui/markdown/GitlabRestMarkDownProcessorTest.kt
+++ src/test/kotlin/com/github/idell/gitlabconnect/ui/markdown/GitlabRestMarkDownProcessorTest.kt
@@ -5,9 +5,11 @@ import com.github.idell.gitlabconnect.gitlab.ProjectInfo
 import com.github.idell.gitlabconnect.storage.GitlabConnectGlobalSettings
 import com.github.idell.gitlabconnect.storage.SecureTokenStorage
 import org.assertj.core.api.Assertions.assertThat
+import org.junit.Ignore
 import org.junit.jupiter.api.Test
 
 internal class GitlabRestMarkDownProcessorTest {
+    @Ignore
     @Test
     internal fun process() {
 
