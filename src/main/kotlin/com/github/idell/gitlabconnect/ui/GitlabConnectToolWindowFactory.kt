6f5b97a0dce33c180feb68e121ad5e2c53dec2ca
gpanice <giovanni.panice@lastminute.com>
2021-04-23 20:03:27 +0200

    some refactoring

--- src/main/kotlin/com/github/idell/gitlabconnect/ui/GitlabConnectToolWindowFactory.kt
+++ src/main/kotlin/com/github/idell/gitlabconnect/ui/GitlabConnectToolWindowFactory.kt
@@ -1,11 +1,7 @@
 package com.github.idell.gitlabconnect.ui
 
 import com.github.idell.gitlabconnect.GitlabConnectBundle
-import com.github.idell.gitlabconnect.exception.GitlabProcessException
-import com.github.idell.gitlabconnect.gitlab.GitlabTokenConfiguration
 import com.github.idell.gitlabconnect.services.gitlab.GitlabConnectApiService
-import com.github.idell.gitlabconnect.storage.GitlabConnectGlobalSettings
-import com.github.idell.gitlabconnect.storage.SecureTokenStorage
 import com.github.idell.gitlabconnect.ui.issue.listener.ShowIssueListener
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
@@ -14,8 +10,6 @@ import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.content.ContentFactory
 
 class GitlabConnectToolWindowFactory : ToolWindowFactory {
-    private val globalSettings = GitlabConnectGlobalSettings.getInstance()
-    private val tokenStorage = SecureTokenStorage()
 
     override fun createToolWindowContent(project: Project, toolWindow: ToolWindow) {
 
@@ -24,25 +18,17 @@ class GitlabConnectToolWindowFactory : ToolWindowFactory {
             ShowIssueListener(),
             project.service<GitlabConnectApiService>().getApi(),
         )
-        val contentFactory = ContentFactory.SERVICE.getInstance()
-        val content = contentFactory.createContent(
+
+        val content = ContentFactory.SERVICE.getInstance().createContent(
             gitlabConnectToolWindow.geContent(),
             GitlabConnectBundle.message(ISSUES_TAB_NAME),
             false
         )
+
         toolWindow.contentManager.addContent(content)
     }
 
-    private fun retrieveGitlabTokenConfiguration(): GitlabTokenConfiguration {
-        val host = globalSettings.state.tokenConfig.host
-        val token = tokenStorage.getToken(host).orElseThrow {
-            GitlabProcessException(GitlabConnectBundle.message(TOKEN_NOT_FOUND_MESSAGE, host))
-        }
-        return GitlabTokenConfiguration(host, token)
-    }
-
     companion object {
-        private const val ISSUES_TAB_NAME = "ui.tab.isssue.name"
-        private const val TOKEN_NOT_FOUND_MESSAGE = "storage.token.not-found"
+        private const val ISSUES_TAB_NAME = "ui.tab.issue.name"
     }
 }
